8dac0b2ed3ccb48ecd651f56e14ba8355b2959a0
ReyhanehNasseri <nasserireyhane@gmail.com>
2024-11-18 18:28:38 +0330

    updated methods

--- repository/customuserrepository.go
+++ repository/customuserrepository.go
@@ -11,7 +11,7 @@ type CustomUserRepository struct {
 var userredisrepo = new(RedisUserRepository)
 var usermysqlrepo = new(MysqlUserRepository)
 
-func (customrepo CustomUserRepository) Connect() error {
+func (customrepo *CustomUserRepository) Connect() error {
 	if err1 := usermysqlrepo.Connect(); err1 != nil {
 		return err1
 	} else {
@@ -25,7 +25,7 @@ func (customrepo CustomUserRepository) Connect() error {
 	return nil
 }
 
-func (customrepo CustomUserRepository) InsertUser(user model.User) (model.User, error) {
+func (customrepo *CustomUserRepository) InsertUser(user model.User) (model.User, error) {
 	if _, err1 := userredisrepo.InsertUser(user); err1 != nil {
 		fmt.Println(err1)
 	} else {
@@ -39,7 +39,7 @@ func (customrepo CustomUserRepository) InsertUser(user model.User) (model.User, 
 	return user, nil
 }
 
-func (customrepo CustomUserRepository) UpdateUser(id string, newFirstname string, newLastname string) error {
+func (customrepo *CustomUserRepository) UpdateUser(id string, newFirstname string, newLastname string) error {
 	if err1 := usermysqlrepo.UpdateUser(id, newFirstname, newLastname); err1 != nil {
 		fmt.Println(err1)
 	} else {
@@ -48,13 +48,13 @@ func (customrepo CustomUserRepository) UpdateUser(id string, newFirstname string
 	if err2 := userredisrepo.UpdateUser(id, newFirstname, newLastname); err2 != nil {
 		fmt.Println(err2)
 	} else {
-		fmt.Println("Updated user in mysql")
+		fmt.Println("Updated user in redis")
 	}
 	return nil
 
 }
 
-func (customrepo CustomUserRepository) DeleteUser(id string) error {
+func (customrepo *CustomUserRepository) DeleteUser(id string) error {
 	if err1 := usermysqlrepo.DeleteUser(id); err1 != nil {
 		fmt.Println(err1)
 	} else {
@@ -63,12 +63,12 @@ func (customrepo CustomUserRepository) DeleteUser(id string) error {
 	if err2 := (userredisrepo).DeleteUser(id); err2 != nil {
 		fmt.Println(err2)
 	} else {
-		fmt.Println("Deleted user in mysql")
+		fmt.Println("Deleted user in redis")
 	}
 	return nil
 }
 
-func (customrepo CustomUserRepository) Read(id string) error {
+func (customrepo *CustomUserRepository) Read(id string) error {
 	err := (userredisrepo).Read(id)
 	if err != nil {
 		err = (usermysqlrepo).Read(id)
@@ -76,6 +76,6 @@ func (customrepo CustomUserRepository) Read(id string) error {
 	return err
 }
 
-func (customrepo CustomUserRepository) Read_all() {
+func (customrepo *CustomUserRepository) Read_all() {
 	(usermysqlrepo).Read_all()
 }
